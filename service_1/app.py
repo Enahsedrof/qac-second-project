7033fe093ca4b44fa90f68376af9a846835a0bef
Shane Forde <13754903+Dunmersc2@users.noreply.github.com>
2021-06-08 15:03:30 +0100

    added create.py

--- service_1/app.py
+++ service_1/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 import requests
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://data.db' #getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+class Workouts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False)
+    exercise = db.Column(db.String(30), nullable=False)
+
 
 @app.route('/')
 def index():
@@ -10,11 +21,14 @@ def index():
 
     payload = {'rep': rep, 'exercise': exercise}
     workout = requests.post('https://service-4:5000/post/workout', json=payload).json()
-    #db.session
-    #db.commit
-    #workout.query.all()
+    
+    db.session.add(Rep(number = rep, exercise = exercise )
+    db.session.commit
+    all_workouts= Workouts.query.order_by(desc(Workouts.id)).limit(5).all()
 
-    return f"You must do {rep} {exercise} to complete your workout for {workout} calories burned.\n"
+    return render_template("index.html", rep=rep, exercise=exercise, all_workouts=all_workouts)
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+    # return f"You must do {rep} {exercise} to complete your workout for {workout} calories burned.\n"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 5000, debug=True)
