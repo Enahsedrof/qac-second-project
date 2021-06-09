64149c39bcf875d441ff196af7389ff895df2fbd
Shane Forde <shaneforde02@googlemail.com>
2021-06-09 17:32:22 +0100

    tidied up

--- service_1/app.py
+++ service_1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 import requests
